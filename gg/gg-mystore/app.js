c2076acb606414a91593ef31492ba60331e69764
flukgaro <pitpagon@hotmail.com>
2020-05-19 09:20:24 +0700

    new header

---
+++ gg/gg-mystore/app.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+//passport ID
+const passport = require("passport")
+const passportLocal = require("passport-local")
+const bcrypt = require('bcryptjs')
+const path = require("path");
+const session = require("express-session")
+const flash = require("connect-flash")
+const multer = require('multer');
+let sneaker = require("./model/sneaker")
+let User = require("./model/db");
+let user = require('./user');
+let Sneaker = require("./admin")
+let app = express()
+
+
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  }))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+passport.use('local', new passportLocal(User.authenticate()));
+passport.serializeUser(function(user, done) { 
+    done(null, user);
+  });
+  passport.deserializeUser(function(user, done) {
+    if(user!=null)
+      done(null,user);
+      
+  });
+
+
+app.get("*",function(req , res , next){
+    res.locals.user = req.user || null
+    res.locals.error = req.flash("error")
+    res.locals.success = req.flash("success")
+    res.locals.Sneaker = req.Sneaker
+    next()
+})
+app.use('/', user);
+app.use('/', Sneaker);
+app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
+app.set('views',[path.join(__dirname,"views"),
+                 path.join(__dirname,"views/routes") ]);
+
+
+
+
+
+
+                 
+app.get("/admin/editlist",function(req,res){
+res.render("editlist");
+});     
+                
+
+app.get("/adidas/details",function(req,res){
+    res.render("details2");
+});     
+
+app.get("/nike/details",function(req,res){
+    res.render("details2");
+});  
+
+app.get("/jordan/details",function(req,res){
+    res.render("details2");
+});  
+
+    // app.get("/add",function(req,res){
+    //     res.render("add");
+    // });
+
+app.get("/edit",function(req,res){
+    res.render("edit");
+});  
+app.get("/1",function(req,res){
+    res.render("profile");
+});  
+
+app.get("/nike/details/checkbil",function(req,res){
+    res.render("checkbil");
+});  
+
+app.get("/adidas/details/checkbil",function(req,res){
+    res.render("checkbil");
+});  
+
+
+app.get("/jordan/details/checkbil",function(req,res){
+    res.render("checkbil");
+});  
+
+// app.get("/profile",function(req,res){
+//     res.render("profile");
+// });
+// app.get("/profile2",function(req,res){
+//     res.render("profile2");
+// });    
+
+
+
+
+// let lolskin = [
+//         {name: "Sejuani",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/3/32/Sejuani_HextechSkin.jpg/revision/latest?cb=20200219200507"},
+//         {name: "Ekko",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/c/c8/Ekko_TrueDamageSkin.jpg/revision/latest?cb=20191030033742"},
+//         {name: "Thresh",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/c/c4/Thresh_DarkStarSkin.jpg/revision/latest?cb=20181021072919"},
+//         {name: "Annie",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/e/e6/Annie_Annie-VersarySkin.jpg/revision/latest?cb=20191017202438"},
+//         {name: "Warwick",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/2/22/Warwick_UrfwickSkin.jpg/revision/latest?cb=20181021045405"},
+//         {name: "Miss Fortune",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/d/dc/Miss_Fortune_PrestigeBewitchingSkin.jpg/revision/latest?cb=20191010151648"},
+//         {name: "Yuumi",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/3/33/Yuumi_HeartseekerSkin.jpg/revision/latest?cb=20200123005349"},
+//         {name: "Garen",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/2/25/Garen_MechaKingdomsSkin.jpg/revision/latest?cb=20191212013930"},
+//         {name: "Lux",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/2/25/Lux_ElementalistSkin.jpg/revision/latest?cb=20181021021002"},
+//         {name: "Anivia",imgurl:"https://vignette.wikia.nocookie.net/leagueoflegends/images/1/15/Anivia_PapercraftSkin.jpg/revision/latest?cb=20190208202530"},
+//       ];
+
+app.get("/edit", function(req,res){
+    res.render("edit");
+});
+
+app.get("/jordan", function(req,res){
+    res.render("jordan");
+});
+
+app.get("/home2", function(req,res){
+    res.render("home2");
+});
+
+
+app.get("/nike",function(req,res){
+    res.render("nike");
+});
+
+// app.get("/signup",function(req,res){
+//     res.render("signup");
+// });
+
+app.listen(3000, function(req,res){
+    console.log("Started Now!!");
+});
+
+app.get("/adidas",function(req,res){
+    res.render("adidas");
+});
+
+
+
+// User 
---
+++ gg/gg-mystore/user.js
@@ -0,0 +1,185 @@
+const express = require("express");
+var router = express.Router();
+const {check,validationResult}= require("express-validator");
+let User = require("./model/db");
+let sneaker = require("./model/sneaker")
+let bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const passport = require("passport")
+const passportLocal = require('passport-local');
+
+
+const multer = require('multer');
+
+let app = express()
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+passport.use('local', new passportLocal(User.authenticate()));
+passport.serializeUser(function(user, done) { 
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  if(user!=null)
+    done(null,user);
+    
+});
+app.use((req,res,next)=>{
+  res.locals.user = req.user || null
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  
+  next();
+})
+
+let saveprofile = multer.diskStorage(
+  {
+  destination:function(req,file,cb){
+    cb(null,"./public/images/img-profile/");
+  },
+  filename:function(req,file,cb){
+   
+
+    cb(null,file.originalname);
+  }
+})
+
+let edit = multer({storage : saveprofile});
+
+
+
+router.get('/', function(req, res, next) {
+  res.render('home2');
+});
+function enSureAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+        return next();
+    }else{
+      req.flash('error', 'You need to login first');
+      res.redirect("/login")
+      
+}
+}
+
+router.get("/signup",function(req , res, ){
+    res.render("signup2");
+})
+router.get("/login",function(req , res, ){
+    res.render("login2");
+})
+router.get('/logout', function(req, res) {
+  req.logout();
+  req.flash("success","logout successfully")
+  res.redirect('/');
+});
+
+router.get("/profile",function(req , res){
+  User.findById({_id:req.user._id},function(error,user){
+    if(error){
+      console.log(error)
+    }else{
+      res.render("profile2",{user:user});
+    }
+  })
+
+  }  )
+
+
+
+
+router.post("/login",passport.authenticate("local",{
+          failureRedirect:"/login",
+          successRedirect : "/profile",
+          successFlash: true,            
+         failureFlash: true,
+          successFlash: "You log in successfully",
+         failureFlash: "Invalid username or password "
+}),
+function(req , res){
+})
+
+
+
+
+router.post('/signup',function(req,res){
+  User.register(new User({username:req.body.username,email:req.body.email,name :"",Surename :"",Housenumber:"", Province:"", District:"", Postalcode:"", IDCard :"", Telephone:"", Size :"",Gender:"", image:""
+    }), req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          req.flash('error','Username or Email had already used');
+          return res.redirect('/signup')
+      }
+      passport.authenticate('local')(req,res,function(){
+          req.flash('success','You Signup in successfully');
+         res.redirect('/')
+      });
+  });
+});
+    
+
+
+
+
+    router.get('/profile/:id/edit', function(req,res){
+      User.findById({_id:req.user._id},function(err, edit){
+          if(err){
+              console.log("error");
+          } else {
+              res.render('edit',{edit:edit});
+          }
+      })
+  });
+  
+  router.post('/profile/:id/edit',edit.single('image'),function(req,res){
+    if(!req.file){
+        console.log(req.file)
+    let Name = req.body.Name
+    let Surename = req.body.Surename
+    let Housenumber = req.body.Housenumber
+    let Province= req.body.Province
+    let District= req.body.District
+    let Postalcode= req.body.Postalcode
+    let IDCard= req.body.IDCard
+    let Telephone= req.body.Telephone
+    let Size= req.body.Size
+    let Gender= req.body.Gender
+    let image= req.body.image
+          
+   
+    User.updateMany({_id:req.user._id},{$set : {Name:Name,Surename:Surename,Housenumber:Housenumber,Province:Province
+    ,District:District,Postalcode:Postalcode,IDCard:IDCard,Telephone:Telephone,Size:Size,Gender:Gender,image:image}} ,function(err, update){
+        if(err){
+            console.log(error);
+        } else {
+             req.flash('success','Edit profile success');
+            res.redirect('/profile')
+            }
+        }
+    );
+}
+if(req.file){
+  let Name = req.body.Name;
+  let Surename = req.body.Surename;
+  let Housenumber = req.body.Housenumber
+  let Province= req.body.Province
+  let District= req.body.District
+  let Postalcode= req.body.Postalcode
+  let IDCard= req.body.IDCard
+  let Telephone= req.body.Telephone
+  let Size= req.body.Size
+  let Gender= req.body.Gender
+  let image= req.file.image
+    User.updateMany({_id:req.user._id},{$set : {Name:Name,Surname:Surname,Housenumber:Housenumber,Province:Province
+      ,District:District,Postalcode:Postalcode,IDCard:IDCard,Telephone:Telephone,Size:Size,Gender:Gender,image:image}} ,function(err, update){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/profile')
+            }
+        }
+    );
+}
+});
+  
+  module.exports = router;
